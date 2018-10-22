3aab5593e8d8cf03c5cd2b8894cb6040e851ebb1
AdrianSalas98 <adriansalase@gmail.com>
2018-10-22 18:52:33 +0200

    Ejercicios 9 y 10 añadidos al codigo

--- src/interfaces/Datos_Cliente.java
+++ src/interfaces/Datos_Cliente.java
@@ -6,21 +6,30 @@ import javax.swing.JFrame;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JRadioButton;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import com.toedter.calendar.JDateChooser;
 import javax.swing.JButton;
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+import javax.swing.Action;
+import javax.swing.ButtonGroup;
+import java.awt.event.ActionListener;
 
 public class Datos_Cliente {
 
 	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
+	private JTextField txtNombre;
+	private JTextField txtPrimerApellido;
+	private JTextField txtSegundoApellido;
+	private JTextField txtDireccion;
+	private JTextField txtCorreo;
+	private final Action action = new SwingAction();
+	private final ButtonGroup buttonGroup = new ButtonGroup();
 
 	/**
 	 * Launch the application.
@@ -53,95 +62,107 @@ public class Datos_Cliente {
 		frame.setBounds(100, 100, 530, 619);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 24));
+		JLabel labelDatosCliente = new JLabel("New label");
+		labelDatosCliente.setFont(new Font("Tahoma", Font.BOLD, 24));
 		
-		JLabel lblNewLabel_1 = new JLabel("New label");
+		JLabel labelNombre = new JLabel("New label");
 		
-		textField = new JTextField();
-		textField.setColumns(10);
+		txtNombre = new JTextField();
+		txtNombre.setColumns(10);
 		
-		JLabel lblNewLabel_2 = new JLabel("New label");
+		JLabel labelPrimerApellido = new JLabel("New label");
 		
-		JLabel lblNewLabel_3 = new JLabel("New label");
+		JLabel labelSegundoApellido = new JLabel("New label");
 		
-		JLabel lblNewLabel_4 = new JLabel("New label");
+		JLabel labelDireccion = new JLabel("New label");
 		
-		JLabel lblNewLabel_5 = new JLabel("New label");
+		JLabel labelCorreo = new JLabel("New label");
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
+		txtPrimerApellido = new JTextField();
+		txtPrimerApellido.setColumns(10);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
+		txtSegundoApellido = new JTextField();
+		txtSegundoApellido.setColumns(10);
 		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
+		txtDireccion = new JTextField();
+		txtDireccion.setColumns(10);
 		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
+		txtCorreo = new JTextField();
+		txtCorreo.setColumns(10);
 		
-		JLabel lblNewLabel_6 = new JLabel("New label");
+		JLabel labelGenero = new JLabel("New label");
 		
-		JRadioButton rdbtnNewRadioButton = new JRadioButton("New radio button");
+		JRadioButton rbHombre = new JRadioButton("New radio button");
+		buttonGroup.add(rbHombre);
 		
-		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("New radio button");
+		JRadioButton rbMujer = new JRadioButton("New radio button");
+		buttonGroup.add(rbMujer);
 		
-		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("New radio button");
+		JRadioButton rbNoDeterminado = new JRadioButton("New radio button");
+		buttonGroup.add(rbNoDeterminado);
 		
-		JLabel lblNewLabel_7 = new JLabel("New label");
+		JLabel labelFecha = new JLabel("New label");
 		
-		JDateChooser dateChooser = new JDateChooser();
+		JDateChooser dcFechaNacimiento = new JDateChooser();
 		
-		JButton btnNewButton = new JButton("New button");
+		JButton botonGuardar = new JButton("New button");
 		
-		JButton btnNewButton_1 = new JButton("New button");
+		JButton botonSiguiente = new JButton("New button");
+		botonSiguiente.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(txtNombre.getText().equals("") || txtPrimerApellido.getText().equals("") || txtSegundoApellido.getText().equals("") || txtDireccion.getText().equals("") || txtCorreo.getText().equals("")) {
+					JOptionPane.showMessageDialog(null, "Hay campos obligatorios sin rellenar", "Error",
+							JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
 		
-		JLabel lblNewLabel_8 = new JLabel("New label");
+		
+		JLabel labelUsuario = new JLabel("New label");
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addGap(40)
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 161, GroupLayout.PREFERRED_SIZE)
+						.addComponent(botonGuardar, GroupLayout.PREFERRED_SIZE, 161, GroupLayout.PREFERRED_SIZE)
 						.addGroup(groupLayout.createSequentialGroup()
 							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addComponent(lblNewLabel_1)
-								.addComponent(lblNewLabel_2)
-								.addComponent(lblNewLabel_3)
-								.addComponent(lblNewLabel_4)
-								.addComponent(lblNewLabel_5)
-								.addComponent(lblNewLabel_6)
-								.addComponent(lblNewLabel_7))
+								.addComponent(labelNombre)
+								.addComponent(labelPrimerApellido)
+								.addComponent(labelSegundoApellido)
+								.addComponent(labelDireccion)
+								.addComponent(labelCorreo)
+								.addComponent(labelGenero)
+								.addComponent(labelFecha))
 							.addPreferredGap(ComponentPlacement.RELATED)
 							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 								.addGroup(groupLayout.createSequentialGroup()
 									.addGap(73)
 									.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
-										.addComponent(textField_1)
-										.addComponent(textField_2)
-										.addComponent(textField_3)
-										.addComponent(textField_4)
-										.addComponent(textField, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 315, Short.MAX_VALUE)))
+										.addComponent(txtPrimerApellido)
+										.addComponent(txtSegundoApellido)
+										.addComponent(txtDireccion)
+										.addComponent(txtCorreo)
+										.addComponent(txtNombre, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 315, Short.MAX_VALUE)))
 								.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-									.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 160, GroupLayout.PREFERRED_SIZE)
+									.addComponent(botonSiguiente, GroupLayout.PREFERRED_SIZE, 160, GroupLayout.PREFERRED_SIZE)
 									.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
 										.addGap(18)
 										.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
-											.addComponent(dateChooser, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+											.addComponent(dcFechaNacimiento, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 											.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
-												.addComponent(rdbtnNewRadioButton)
+												.addComponent(rbHombre)
 												.addPreferredGap(ComponentPlacement.RELATED)
-												.addComponent(rdbtnNewRadioButton_1)
+												.addComponent(rbMujer)
 												.addPreferredGap(ComponentPlacement.RELATED)
-												.addComponent(rdbtnNewRadioButton_2))))))))
+												.addComponent(rbNoDeterminado))))))))
 					.addContainerGap(40, Short.MAX_VALUE))
 				.addGroup(groupLayout.createSequentialGroup()
 					.addGap(91)
-					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 164, GroupLayout.PREFERRED_SIZE)
+					.addComponent(labelDatosCliente, GroupLayout.PREFERRED_SIZE, 164, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED, 116, Short.MAX_VALUE)
-					.addComponent(lblNewLabel_8)
+					.addComponent(labelUsuario)
 					.addGap(97))
 		);
 		groupLayout.setVerticalGroup(
@@ -149,46 +170,54 @@ public class Datos_Cliente {
 				.addGroup(groupLayout.createSequentialGroup()
 					.addGap(28)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNewLabel_8))
+						.addComponent(labelDatosCliente, GroupLayout.PREFERRED_SIZE, 36, GroupLayout.PREFERRED_SIZE)
+						.addComponent(labelUsuario))
 					.addGap(59)
 					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addComponent(lblNewLabel_1)
-						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(labelNombre)
+						.addComponent(txtNombre, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(35)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel_2)
-						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(labelPrimerApellido)
+						.addComponent(txtPrimerApellido, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(36)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel_3)
-						.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(labelSegundoApellido)
+						.addComponent(txtSegundoApellido, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(34)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel_4)
-						.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(labelDireccion)
+						.addComponent(txtDireccion, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(31)
 					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addComponent(lblNewLabel_5)
-						.addComponent(textField_4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(labelCorreo)
+						.addComponent(txtCorreo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(41)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel_6)
-						.addComponent(rdbtnNewRadioButton)
-						.addComponent(rdbtnNewRadioButton_1)
-						.addComponent(rdbtnNewRadioButton_2))
+						.addComponent(labelGenero)
+						.addComponent(rbHombre)
+						.addComponent(rbMujer)
+						.addComponent(rbNoDeterminado))
 					.addGap(25)
 					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addComponent(lblNewLabel_7)
-						.addComponent(dateChooser, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(labelFecha)
+						.addComponent(dcFechaNacimiento, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(33)
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(btnNewButton_1, GroupLayout.DEFAULT_SIZE, 41, Short.MAX_VALUE)
-						.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, 41, Short.MAX_VALUE))
+						.addComponent(botonSiguiente, GroupLayout.DEFAULT_SIZE, 41, Short.MAX_VALUE)
+						.addComponent(botonGuardar, GroupLayout.DEFAULT_SIZE, 41, Short.MAX_VALUE))
 					.addGap(36))
 		);
 		frame.getContentPane().setLayout(groupLayout);
 	}
 	
 	
+	private class SwingAction extends AbstractAction {
+		public SwingAction() {
+			putValue(NAME, "SwingAction");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
 }
